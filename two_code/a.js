0f4129758e45eab0ce49b20d1001bfbd983b6c00
yangchunxiang <yangchunxiang@afcat.com.cn>
2020-01-05 18:24:29 +0800

    二维码插件

--- two_code/a.js
+++ two_code/a.js
@@ -1,14 +1,11 @@
 window.onload = function(){
-    
-
- 
     var qrcode = new QRCode(document.getElementById("qrcode"), {
         width : 100,
         height : 100
     });
-    
+
+    var elText = document.getElementById("text");
     function makeCode () {		
-        var elText = document.getElementById("text");
         
         if (!elText.value) {
             alert("Input a text");
@@ -21,15 +18,29 @@ window.onload = function(){
     
     makeCode();
     
-    $("#text").
-        on("blur", function () {
+
+    //提示框输入地址jquery写法
+    // $("#text").
+    //     on("blur", function () {
+    //         makeCode();
+    //     }).
+    //     on("keydown", function (e) {
+    //         if (e.keyCode == 13) {
+    //             makeCode();
+    //         }
+    //     });
+
+     //提示框输入地址原生写法
+     //失去焦点
+     elText.onblur = function(){
+        makeCode();
+     };
+     elText.onkeydown = function(e){
+        if (e.keyCode == 13) {
             makeCode();
-        }).
-        on("keydown", function (e) {
-            if (e.keyCode == 13) {
-                makeCode();
-            }
-        });
+        }
+     };
+   
         
 }
  
