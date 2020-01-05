041c2ff3ab8cf437c1f199ccb0f40ecac192b79e
yangchunxiang <yangchunxiang@afcat.com.cn>
2020-01-05 18:40:56 +0800

    towcode - branch

---
+++ two_code - ceshi/a.js
@@ -0,0 +1,46 @@
+window.onload = function(){
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+        width : 100,
+        height : 100
+    });
+
+    var elText = document.getElementById("text");
+    function makeCode () {		
+        
+        if (!elText.value) {
+            alert("Input a text");
+            elText.focus();
+            return;
+        }
+        
+        qrcode.makeCode(elText.value);
+    }
+    
+    makeCode();
+    
+
+    //提示框输入地址jquery写法
+    // $("#text").
+    //     on("blur", function () {
+    //         makeCode();
+    //     }).
+    //     on("keydown", function (e) {
+    //         if (e.keyCode == 13) {
+    //             makeCode();
+    //         }
+    //     });
+
+     //提示框输入地址原生写法
+     //失去焦点
+     elText.onblur = function(){
+        makeCode();
+     };
+     elText.onkeydown = function(e){
+        if (e.keyCode == 13) {
+            makeCode();
+        }
+     };
+    
+        
+}
+ 
